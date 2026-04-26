41e40590c3a932138a1315ddfe28651073e6c4d5
jason13official <jason13official@gmail.com>
2026-04-26 15:19:08 -0400

    i mean yeah

---
+++ common/src/main/java/io/github/jason13official/animal_armor_trims/mixin/LivingEntityRendererMixin.java
@@ -0,0 +1,37 @@
+package io.github.jason13official.animal_armor_trims.mixin;
+
+import io.github.jason13official.animal_armor_trims.impl.client.api.renderer.entity.state.HorseArmorRenderStateAccessor;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.state.HorseRenderState;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.equine.Horse;
+import net.minecraft.world.item.equipment.trim.ArmorTrim;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntityRenderer.class)
+public abstract class LivingEntityRendererMixin<T extends LivingEntity, S extends LivingEntityRenderState, M extends EntityModel<? super S>>
+    extends EntityRenderer<T, S> implements RenderLayerParent<S, M> {
+
+  protected LivingEntityRendererMixin(Context context) {
+    super(context);
+  }
+
+  @Inject(at = @At("TAIL"), method = "extractRenderState(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;F)V")
+  private void animal_armor_trims$extractRenderState(T entity, S state, float partialTicks, CallbackInfo ci) {
+    if (!(entity instanceof Horse horse) || !(state instanceof HorseRenderState horseRenderState)) return;
+
+    if (horse.getBodyArmorItem().has(DataComponents.TRIM)) {
+      ArmorTrim trim = horse.getBodyArmorItem().get(DataComponents.TRIM);
+      ((HorseArmorRenderStateAccessor) horseRenderState).animal_armor_trims$setArmorTrim(trim);
+    }
+  }
+}
